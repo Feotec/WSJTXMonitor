7562687ab0bfb307239d3167413749fbeb9dbf7b
Feotec <feotec@gmail.com>
2025-09-28 20:11:58 -0700

    Added license notice to top of file

--- app/src/main/java/com/feotec/wsjt_xmonitor/TouchableTextView.java
+++ app/src/main/java/com/feotec/wsjt_xmonitor/TouchableTextView.java
@@ -1,4 +1,20 @@
 package com.feotec.wsjt_xmonitor;
+/*
+ * Copyright (C) 2019-2025 Feotec Thomas Reynolds
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation GNU APGLv3 or later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * A copy of GNU APGLv3, the GNU General Public license is in a file
+ * named COPYING located in the root directory.
+ */
+
 
 import android.content.Context;
 import androidx.appcompat.widget.AppCompatTextView;
